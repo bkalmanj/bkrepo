698024dba44832edf0ec6fb7cb150bd6132d5b66
bkalmanj <58172525+bkalmanj@users.noreply.github.com>
2023-03-05 18:18:47 +0530

    added filename and key as arguments

--- nest.py
+++ nest.py
@@ -1,32 +1,34 @@
-#!/usr/bin/python3
+
 import json
 import sys
-
+args = sys.argv[1:]
+json_file = args[0]
+key_json = args[1] 
 
 def key_extract(obj, key):
-    arr = []
+    arr = []
+    def extract(obj, arr, key):     
+        if isinstance(obj, dict):
+            for k, v in obj.items():      
+                if k == key and isinstance(v,(dict, list)):
+                    arr.append(v)               
+                elif isinstance(v, (dict, list)):
+                    extract(v, arr, key)
+                elif k == key:
+                    arr.append(v)                    
+        elif isinstance(obj, list):
+            for item in obj:
+                extract(item, arr, key)
+        return arr
 
-    def extract(obj, arr, key):
-        if isinstance(obj, dict):
-            for k, v in obj.items():      
-                if k == key and isinstance(v,(dict, list)):
-                    arr.append(v)               
-                elif isinstance(v, (dict, list)):
-                    extract(v, arr, key)
-                elif k == key:
-                    arr.append(v)                    
-        elif isinstance(obj, list):
-            for item in obj:
-                extract(item, arr, key)
-        return arr
+    values = extract(obj, arr, key)
+    if isinstance(values, (dict, list)):
+        return json.dumps(values,indent=4)
+    else:
+        return values
+    
 
-    values = extract(obj, arr, key)
-    if isinstance(values, (dict, list)):
-        return json.dumps(values,indent=4)
-    else:
-        return values
-    
-if __name__ == '__main__':
-    obj = {'a': {'b': {'c': 'd'}}}
-    value = key_extract(obj, 'c')
-    print(value)
+with open(json_file) as f:
+   obj = json.load(f)
+value = key_extract(obj, key_json)
+print(value)
